ac8e17187e70e513f8a9c8c42c75026383b3784a
nikstormCh <116068053+nikstormCh@users.noreply.github.com>
2022-12-05 19:55:24 +0200

    Add files via upload

---
+++ JS_project_dice_1.js
@@ -0,0 +1,579 @@
+function rnd(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+function random_up(){
+    let arv1, arv2, arv3, arv4, arv5, arv6;
+    arv1 = rnd(1, 7);
+    if (arv1 == 1){
+        document.getElementById("arv1").innerHTML = "suck";
+    }
+    if (arv1 == 2){
+        document.getElementById("arv1").innerHTML = "tickle";
+    }
+    if (arv1 == 3){
+        document.getElementById("arv1").innerHTML = "lick";
+    }
+    if (arv1 == 4){
+        document.getElementById("arv1").innerHTML = "bite";
+    }
+    if (arv1 == 5){
+        document.getElementById("arv1").innerHTML = "kiss";
+    }
+    if (arv1 == 6){
+        document.getElementById("arv1").innerHTML = "massage";
+    }
+    arv2 = rnd(1, 8);
+    if (arv2 == 1){
+        document.getElementById("arv2").innerHTML = "ears";
+    }
+    if (arv2 == 2){
+        document.getElementById("arv2").innerHTML = "toes";
+    }
+    if (arv2 == 3){
+        document.getElementById("arv2").innerHTML = "fingers";
+    }
+    if (arv2 == 4){
+        document.getElementById("arv2").innerHTML = "boobs";
+    }
+    if (arv2 == 5){
+        document.getElementById("arv2").innerHTML = "ass";
+    }
+    if (arv2 == 6){
+        document.getElementById("arv2").innerHTML = "lips";
+    }
+
+    if (arv2 == 7){
+        document.getElementById("arv2").innerHTML = "vagina";
+    }
+
+    arv3 = rnd(1, 9);
+    if (arv3 == 1){
+        document.getElementById("arv3").innerHTML = "30 sec";
+    }
+    if (arv3 == 2){
+        document.getElementById("arv3").innerHTML = "45 sec";
+    }
+    if (arv3 == 3){
+        document.getElementById("arv3").innerHTML = "1 min";
+    }
+    if (arv3 == 4){
+        document.getElementById("arv3").innerHTML = "1.5 min";
+    }
+    if (arv3 == 5){
+        document.getElementById("arv3").innerHTML = "2 min";
+    }
+    if (arv3 == 6){
+        document.getElementById("arv3").innerHTML = "2.5 min";
+    }
+    if (arv3 == 7){
+        document.getElementById("arv3").innerHTML = "3 min";
+    }
+    if (arv3 == 8){
+        document.getElementById("arv3").innerHTML = "5 min";
+    }
+
+    arv4 = rnd(1, 7);
+    if (arv4 == 1){
+        document.getElementById("arv4").innerHTML = "suck";
+    }
+    if (arv4 == 2){
+        document.getElementById("arv4").innerHTML = "tickle";
+    }
+    if (arv4 == 3){
+        document.getElementById("arv4").innerHTML = "lick";
+    }
+    if (arv4 == 4){
+        document.getElementById("arv4").innerHTML = "bite";
+    }
+    if (arv4 == 5){
+        document.getElementById("arv4").innerHTML = "kiss";
+    }
+    if (arv4 == 6){
+        document.getElementById("arv4").innerHTML = "massage";
+    }
+
+    arv5 = rnd(1, 8);
+    if (arv2 == 1){
+        document.getElementById("arv5").innerHTML = "ears";
+    }
+    if (arv5 == 2){
+        document.getElementById("arv5").innerHTML = "toes";
+    }
+    if (arv5 == 3){
+        document.getElementById("arv5").innerHTML = "fingers";
+    }
+    if (arv5 == 4){
+        document.getElementById("arv5").innerHTML = "boobs";
+    }
+    if (arv5 == 5){
+        document.getElementById("arv5").innerHTML = "ass";
+    }
+    if (arv5 == 6){
+        document.getElementById("arv5").innerHTML = "lips";
+    }
+    if (arv5 == 7){
+        document.getElementById("arv5").innerHTML = "vagina";
+    }
+
+    arv6 = rnd(1, 9);
+    if (arv6 == 1){
+        document.getElementById("arv6").innerHTML = "30 sec";
+    }
+    if (arv6 == 2){
+        document.getElementById("arv6").innerHTML = "45 sec";
+    }
+    if (arv6 == 3){
+        document.getElementById("arv6").innerHTML = "1 min";
+    }
+    if (arv6 == 4){
+        document.getElementById("arv6").innerHTML = "1.5 min";
+    }
+    if (arv6 == 5){
+        document.getElementById("arv6").innerHTML = "2 min";
+    }
+    if (arv6 == 6){
+        document.getElementById("arv6").innerHTML = "2.5 min";
+    }
+    if (arv6 == 7){
+        document.getElementById("arv6").innerHTML = "3 min";
+    }
+    if (arv6 == 8){
+        document.getElementById("arv6").innerHTML = "5 min";
+    }
+}
+
+function random_down(){
+    let arv7, arv8, arv9, arv10, arv11, arv12;
+    arv7 = rnd(1, 7);
+    if (arv7 == 1){
+        document.getElementById("arv7").innerHTML = "suck";
+    }
+    if (arv7 == 2){
+        document.getElementById("arv7").innerHTML = "tickle";
+    }
+    if (arv7 == 3){
+        document.getElementById("arv7").innerHTML = "lick";
+    }
+    if (arv7 == 4){
+        document.getElementById("arv7").innerHTML = "bite";
+    }
+    if (arv7 == 5){
+        document.getElementById("arv7").innerHTML = "kiss";
+    }
+    if (arv7 == 6){
+        document.getElementById("arv7").innerHTML = "massage";
+    }
+    arv8 = rnd(1, 9);
+    if (arv8 == 1){
+        document.getElementById("arv8").innerHTML = "ears";
+    }
+    if (arv8 == 2){
+        document.getElementById("arv8").innerHTML = "toes";
+    }
+    if (arv8 == 3){
+        document.getElementById("arv8").innerHTML = "fingers";
+    }
+    if (arv8 == 4){
+        document.getElementById("arv8").innerHTML = "nipples";
+    }
+    if (arv8 == 5){
+        document.getElementById("arv8").innerHTML = "ass";
+    }
+    if (arv8== 6){
+        document.getElementById("arv8").innerHTML = "lips";
+    }
+    if (arv8 == 7){
+        document.getElementById("arv8").innerHTML = "penis";
+    }
+    if (arv8 == 8){
+        document.getElementById("arv8").innerHTML = "balls";
+    }
+
+    arv9 = rnd(1, 9);
+    if (arv9 == 1){
+        document.getElementById("arv9").innerHTML = "30 sec";
+    }
+    if (arv9 == 2){
+        document.getElementById("arv9").innerHTML = "45 sec";
+    }
+    if (arv9 == 3){
+        document.getElementById("arv9").innerHTML = "1 min";
+    }
+    if (arv9 == 4){
+        document.getElementById("arv9").innerHTML = "1.5 min";
+    }
+    if (arv9 == 5){
+        document.getElementById("arv9").innerHTML = "2 min";
+    }
+    if (arv9 == 6){
+        document.getElementById("arv9").innerHTML = "2.5 min";
+    }
+    if (arv9 == 7){
+        document.getElementById("arv9").innerHTML = "3 min";
+    }
+    if (arv9 == 8){
+        document.getElementById("arv9").innerHTML = "5 min";
+    }
+
+    arv10 = rnd(1, 7);
+    if (arv10 == 1){
+        document.getElementById("arv10").innerHTML = "suck";
+    }
+    if (arv10 == 2){
+        document.getElementById("arv10").innerHTML = "tickle";
+    }
+    if (arv10 == 3){
+        document.getElementById("arv10").innerHTML = "lick";
+    }
+    if (arv10 == 4){
+        document.getElementById("arv10").innerHTML = "bite";
+    }
+    if (arv10 == 5){
+        document.getElementById("arv10").innerHTML = "kiss";
+    }
+    if (arv10 == 6){
+        document.getElementById("arv10").innerHTML = "massage";
+    }
+
+    arv11 = rnd(1, 9);
+    if (arv11 == 1){
+        document.getElementById("arv11").innerHTML = "ears";
+    }
+    if (arv11 == 2){
+        document.getElementById("arv11").innerHTML = "toes";
+    }
+    if (arv11 == 3){
+        document.getElementById("arv11").innerHTML = "fingers";
+    }
+    if (arv11 == 4){
+        document.getElementById("arv11").innerHTML = "nipples";
+    }
+    if (arv11 == 5){
+        document.getElementById("arv11").innerHTML = "ass";
+    }
+    if (arv11 == 6){
+        document.getElementById("arv11").innerHTML = "lips";
+    }
+    if (arv11 == 7){
+        document.getElementById("arv11").innerHTML = "penis";
+    }
+    if (arv11 == 8){
+        document.getElementById("arv11").innerHTML = "balls";
+    }
+
+    arv12 = rnd(1, 9);
+    if (arv12 == 1){
+        document.getElementById("arv12").innerHTML = "30 sec";
+    }
+    if (arv12 == 2){
+        document.getElementById("arv12").innerHTML = "45 sec";
+    }
+    if (arv12 == 3){
+        document.getElementById("arv12").innerHTML = "1 min";
+    }
+    if (arv12 == 4){
+        document.getElementById("arv12").innerHTML = "1.5 min";
+    }
+    if (arv12 == 5){
+        document.getElementById("arv12").innerHTML = "2 min";
+    }
+    if (arv12 == 6){
+        document.getElementById("arv12").innerHTML = "2.5 min";
+    }
+    if (arv12 == 7){
+        document.getElementById("arv12").innerHTML = "3 min";
+    }
+    if (arv12 == 8){
+        document.getElementById("arv12").innerHTML = "5 min";
+    }
+}
+
+function random_right(){
+    let arv16, arv17, arv18, arv19;
+
+    arv16 = rnd(1, 9);
+    if (arv16 == 1){
+        document.getElementById("arv16").innerHTML = "30 sec";
+    }
+    if (arv16 == 2){
+        document.getElementById("arv16").innerHTML = "45 sec";
+    }
+    if (arv16 == 3){
+        document.getElementById("arv16").innerHTML = "1 min";
+    }
+    if (arv16 == 4){
+        document.getElementById("arv16").innerHTML = "1.5 min";
+    }
+    if (arv16 == 5){
+        document.getElementById("arv16").innerHTML = "2 min";
+    }
+    if (arv16 == 6){
+        document.getElementById("arv16").innerHTML = "2.5 min";
+    }
+    if (arv16 == 7){
+        document.getElementById("arv16").innerHTML = "3 min";
+    }
+    if (arv16 == 8){
+        document.getElementById("arv16").innerHTML = "5 min";
+    }
+    
+    arv17 = rnd(1, 9);
+    if (arv17 == 1){
+        document.getElementById("arv17").innerHTML = "30 sec";
+    }
+    if (arv17 == 2){
+        document.getElementById("arv17").innerHTML = "45 sec";
+    }
+    if (arv17 == 3){
+        document.getElementById("arv17").innerHTML = "1 min";
+    }
+    if (arv17 == 4){
+        document.getElementById("arv17").innerHTML = "1.5 min";
+    }
+    if (arv17 == 5){
+        document.getElementById("arv17").innerHTML = "2 min";
+    }
+    if (arv17 == 6){
+        document.getElementById("arv17").innerHTML = "2.5 min";
+    }
+    if (arv17 == 7){
+        document.getElementById("arv17").innerHTML = "3 min";
+    }
+    if (arv17 == 8){
+        document.getElementById("arv17").innerHTML = "5 min";
+    }
+    
+    arv18 = rnd(1, 9);
+    if (arv18 == 1){
+        document.getElementById("arv18").innerHTML = "30 sec";
+    }
+    if (arv18 == 2){
+        document.getElementById("arv18").innerHTML = "45 sec";
+    }
+    if (arv18 == 3){
+        document.getElementById("arv18").innerHTML = "1 min";
+    }
+    if (arv18 == 4){
+        document.getElementById("arv18").innerHTML = "1.5 min";
+    }
+    if (arv18 == 5){
+        document.getElementById("arv18").innerHTML = "2 min";
+    }
+    if (arv18 == 6){
+        document.getElementById("arv18").innerHTML = "2.5 min";
+    }
+    if (arv18 == 7){
+        document.getElementById("arv18").innerHTML = "3 min";
+    }
+    if (arv18 == 8){
+        document.getElementById("arv18").innerHTML = "5 min";
+    }
+    
+    arv19 = rnd(1, 7);
+    if (arv19 == 1){
+        document.getElementById("arv19").innerHTML = "kitchen";
+    }
+    if (arv19 == 2){
+        document.getElementById("arv19").innerHTML = "bathroom";
+    }
+    if (arv19 == 3){
+        document.getElementById("arv19").innerHTML = "bed";
+    }
+    if (arv19 == 4){
+        document.getElementById("arv19").innerHTML = "floor";
+    }
+    if (arv19 == 6){
+        document.getElementById("arv19").innerHTML = "chair";
+    }
+    if (arv19 == 7){
+        document.getElementById("arv19").innerHTML = "table";
+    }
+
+    let arv13, arv14, arv15;
+
+    arv13 = rnd(1, 20);
+    if (arv13 == 1){
+        document.getElementById("arv13").src="./small/pos1.png"
+    }
+    if (arv13 == 2){
+        document.getElementById("arv13").src="./small/pos2.png"
+    }
+    if (arv13 == 3){
+        document.getElementById("arv13").src="./small/pos3.png"
+    }
+    if (arv13 == 4){
+        document.getElementById("arv13").src="./small/pos4.png"
+    }
+    if (arv13 == 5){
+        document.getElementById("arv13").src="./small/pos5.png"
+    }
+    if (arv13 == 6){
+        document.getElementById("arv13").src="./small/pos6.png"
+    }
+    if (arv13 == 7){
+        document.getElementById("arv13").src="./small/pos7.png"
+    }
+    if (arv13 == 8){
+        document.getElementById("arv13").src="./small/pos8.png"
+    }
+    if (arv13 == 9){
+        document.getElementById("arv13").src="./small/pos9.png"
+    }
+    if (arv13 == 10){
+        document.getElementById("arv13").src="./small/pos10.png"
+    }
+    if (arv13 == 11){
+        document.getElementById("arv13").src="./small/pos11.png"
+    }
+    if (arv13 == 12){
+        document.getElementById("arv13").src="./small/pos12.png"
+    }
+    if (arv13 == 13){
+        document.getElementById("arv13").src="./small/pos13.png"
+    }
+    if (arv13 == 14){
+        document.getElementById("arv13").src="./small/pos14.png"
+    }
+    if (arv13 == 15){
+        document.getElementById("arv13").src="./small/pos15.png"
+    }
+    if (arv13 == 16){
+        document.getElementById("arv13").src="./small/pos16.png"
+    }
+    if (arv13 == 17){
+        document.getElementById("arv13").src="./small/pos17.png"
+    }
+    if (arv13 == 18){
+        document.getElementById("arv13").src="./small/pos18.png"
+    }
+    if (arv13 == 19){
+        document.getElementById("arv13").src="./small/pos19.png"
+    }
+
+    arv14 = rnd(1, 20);
+    while (arv13 == arv14){
+        arv14 = rnd(1, 20);
+    }
+
+    if (arv14 == 1){
+        document.getElementById("arv14").src="./small/pos1.png"
+    }
+    if (arv14 == 2){
+        document.getElementById("arv14").src="./small/pos2.png"
+    }
+    if (arv14 == 3){
+        document.getElementById("arv14").src="./small/pos3.png"
+    }
+    if (arv14 == 4){
+        document.getElementById("arv14").src="./small/pos4.png"
+    }
+    if (arv14 == 5){
+        document.getElementById("arv14").src="./small/pos5.png"
+    }
+    if (arv14 == 6){
+        document.getElementById("arv14").src="./small/pos6.png"
+    }
+    if (arv14 == 7){
+        document.getElementById("arv14").src="./small/pos7.png"
+    }
+    if (arv14 == 8){
+        document.getElementById("arv14").src="./small/pos8.png"
+    }
+    if (arv14 == 9){
+        document.getElementById("arv14").src="./small/pos9.png"
+    }
+    if (arv14 == 10){
+        document.getElementById("arv14").src="./small/pos10.png"
+    }
+    if (arv14 == 11){
+        document.getElementById("arv14").src="./small/pos11.png"
+    }
+    if (arv14 == 12){
+        document.getElementById("arv14").src="./small/pos12.png"
+    }
+    if (arv14 == 13){
+        document.getElementById("arv14").src="./small/pos13.png"
+    }
+    if (arv14 == 14){
+        document.getElementById("arv14").src="./small/pos14.png"
+    }
+    if (arv14 == 15){
+        document.getElementById("arv14").src="./small/pos15.png"
+    }
+    if (arv14 == 16){
+        document.getElementById("arv14").src="./small/pos16.png"
+    }
+    if (arv14 == 17){
+        document.getElementById("arv14").src="./small/pos17.png"
+    }
+    if (arv14 == 18){
+        document.getElementById("arv14").src="./small/pos18.png"
+    }
+    if (arv14 == 19){
+        document.getElementById("arv14").src="./small/pos19.png"
+    }
+
+    arv15 = rnd(1, 20);
+    while (arv15 == arv14 || arv15 == arv13){
+        arv15 = rnd(1, 20);
+    }
+
+    if (arv15 == 1){
+        document.getElementById("arv15").src="./small/pos1.png"
+    }
+    if (arv15 == 2){
+        document.getElementById("arv15").src="./small/pos2.png"
+    }
+    if (arv15 == 3){
+        document.getElementById("arv15").src="./small/pos3.png"
+    }
+    if (arv15 == 4){
+        document.getElementById("arv15").src="./small/pos4.png"
+    }
+    if (arv15 == 5){
+        document.getElementById("arv15").src="./small/pos5.png"
+    }
+    if (arv15 == 6){
+        document.getElementById("arv15").src="./small/pos6.png"
+    }
+    if (arv15 == 7){
+        document.getElementById("arv15").src="./small/pos7.png"
+    }
+    if (arv15 == 8){
+        document.getElementById("arv15").src="./small/pos8.png"
+    }
+    if (arv15 == 9){
+        document.getElementById("arv15").src="./small/pos9.png"
+    }
+    if (arv15 == 10){
+        document.getElementById("arv15").src="./small/pos10.png"
+    }
+    if (arv15 == 11){
+        document.getElementById("arv15").src="./small/pos11.png"
+    }
+    if (arv15 == 12){
+        document.getElementById("arv15").src="./small/pos12.png"
+    }
+    if (arv15 == 13){
+        document.getElementById("arv15").src="./small/pos13.png"
+    }
+    if (arv15 == 14){
+        document.getElementById("arv15").src="./small/pos14.png"
+    }
+    if (arv15 == 15){
+        document.getElementById("arv15").src="./small/pos15.png"
+    }
+    if (arv15 == 16){
+        document.getElementById("arv15").src="./small/pos16.png"
+    }
+    if (arv15 == 17){
+        document.getElementById("arv15").src="./small/pos17.png"
+    }
+    if (arv15 == 18){
+        document.getElementById("arv15").src="./small/pos18.png"
+    }
+    if (arv15 == 19){
+        document.getElementById("arv15").src="./small/pos19.png"
+    }
+}
